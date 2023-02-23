8f1028c786c3b8a3b243b77ceca01d784aab5b05
Muhammad Saleh <ranaasaleh846@gmail.com>
2023-02-23 21:03:46 +0500

    Expense tracker app

--- client/src/context/GlobalState.jsx
+++ client/src/context/GlobalState.jsx
@@ -21,7 +21,7 @@ export const GlobalProvider = ({ children }) => {
 	async function deleteTransaction(id) {
 		try {
 			await axios.delete(
-				`https://react-expense-tracker-app.vercel.app/api/v1/transactions/${id}`
+				`https://expense-tracker-react-app-sandy.vercel.app/api/v1/transactions/${id}`
 			);
 			dispatch({ type: "DELETE_TRANSACTION", payload: id });
 		} catch (error) {
@@ -40,7 +40,7 @@ export const GlobalProvider = ({ children }) => {
 		};
 		try {
 			const response = await axios.post(
-				"https://react-expense-tracker-app.vercel.app/api/v1/transactions",
+				"https://expense-tracker-react-app-sandy.vercel.app/api/v1/transactions",
 				transaction,
 				config
 			);
@@ -56,7 +56,7 @@ export const GlobalProvider = ({ children }) => {
 	async function getTransactions() {
 		try {
 			const response = await axios.get(
-				"https://react-expense-tracker-app.vercel.app/api/v1/transactions"
+				"https://expense-tracker-react-app-sandy.vercel.app/api/v1/transactions"
 			);
 			dispatch({ type: "GET_TRANSACTIONS", payload: response.data.data });
 		} catch (error) {
